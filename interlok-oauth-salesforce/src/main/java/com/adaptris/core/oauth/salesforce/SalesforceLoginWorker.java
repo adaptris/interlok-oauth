4db27ae2d43b29c318f598305af16525de1a4cd4
Lewin Chan <lewin.chan@adaptris.com>
2019-09-20 15:39:04 +0100

    INTERLOK-2961 - Log the http response if we fail to parse it.
    - Use a custom response handler to capture the response entity
    - Throw an exception if the status code is >=300
    - Log the raw response on exception along with the status-line so you get
    something like
    ```
    Failed to authenticate, got [HTTP/1.1 400 Bad Request],
    HTTP Reply data was : [{"error":"unsupported_grant_type"}]
    ```
    - Make sure that we support lax redirection (since POST generally
    shouldn't default to redirection).
    - increase coverage to 100%

--- interlok-oauth-salesforce/src/main/java/com/adaptris/core/oauth/salesforce/SalesforceLoginWorker.java
+++ interlok-oauth-salesforce/src/main/java/com/adaptris/core/oauth/salesforce/SalesforceLoginWorker.java
@@ -17,18 +17,25 @@
 package com.adaptris.core.oauth.salesforce;
 
 import static org.apache.commons.lang.StringUtils.isBlank;
-
 import java.io.IOException;
-import java.io.InputStream;
-
+import java.io.StringReader;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
+import org.apache.http.ProtocolVersion;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpResponseException;
+import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
-
+import org.apache.http.impl.client.LaxRedirectStrategy;
+import org.apache.http.message.BasicStatusLine;
+import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.adaptris.core.CoreException;
 import com.adaptris.core.http.oauth.AccessToken;
 import com.adaptris.core.util.ExceptionHelper;
@@ -42,28 +49,40 @@ class SalesforceLoginWorker {
 
   private static final String TOKEN_TYPE = "token_type";
   private static final String ACCESS_TOKEN = "access_token";
+  private static final StatusLine DEFAULT_STATUS = new BasicStatusLine(new ProtocolVersion("HTTP", 1, 1), 200, "OK");
+
   private transient String url;
   private transient String proxy;
+
+  private transient Logger log = LoggerFactory.getLogger(SalesforceAccessToken.class);
 
   public SalesforceLoginWorker(String url, String proxy) {
     this.url = url;
     this.proxy = proxy;
   }
 
-  public AccessToken login(HttpEntity entity) throws CoreException {
+  public AccessToken login(HttpEntity request) throws CoreException {
+    String responseBody = "";
+    String httpStatusLine = "";
     try (CloseableHttpClient httpclient = createClient()) {
       HttpPost post = new HttpPost(url);
-      post.setEntity(entity);
-      HttpResponse loginResponse = httpclient.execute(post);
-      return buildToken(loginResponse);
+      post.setEntity(request);
+      // Since BasicResponseHandler throws an exception and doesn't give you the reply data if status >
+      // 300 we need to do exactly what that does.
+      CustomResponseHandler responseHandler = new CustomResponseHandler();
+      responseBody = httpclient.execute(post, responseHandler);
+      httpStatusLine = responseHandler.statusLine();
+      responseHandler.throwExceptionIfAny();
+      return buildToken(responseBody);
     }
     catch (Exception e) {
+      log.error("Failed to authenticate, got [{}], HTTP Reply data was : [{}]", httpStatusLine, responseBody);
       throw ExceptionHelper.wrapCoreException(e);
     }
   }
 
-  AccessToken buildToken(HttpResponse loginResponse) throws UnsupportedOperationException, IOException {
-    try (InputStream in = loginResponse.getEntity().getContent()) {
+  AccessToken buildToken(String response) throws UnsupportedOperationException, IOException {
+    try (StringReader in = new StringReader(response)) {
       ObjectMapper mapper = new ObjectMapper();
       JsonNode loginResult = mapper.readValue(in, JsonNode.class);
       String accessToken = loginResult.get(ACCESS_TOKEN).asText();
@@ -76,12 +95,33 @@ class SalesforceLoginWorker {
   }
 
   CloseableHttpClient createClient() throws Exception {
-    HttpClientBuilder builder = HttpClients.custom();
+    HttpClientBuilder builder = HttpClients.custom().useSystemProperties().setRedirectStrategy(LaxRedirectStrategy.INSTANCE);
     // If someone does config a-la ${http.proxy}:${http.proxy.port} in config with var-sub
     // we end up with : as a proxy...
     if (!isBlank(proxy) && !proxy.equals(":")) {
       builder.setProxy(HttpHost.create(proxy));
     }
     return builder.build();
+  }
+
+  protected static class CustomResponseHandler implements ResponseHandler<String> {
+
+    private StatusLine statusLine = DEFAULT_STATUS;
+
+    @Override
+    public String handleResponse(HttpResponse response) throws ClientProtocolException, IOException {
+      statusLine = response.getStatusLine();
+      return EntityUtils.toString(response.getEntity());
+    }
+
+    protected String statusLine() {
+      return statusLine.toString();
+    }
+    
+    protected void throwExceptionIfAny() throws HttpResponseException {
+      if (statusLine.getStatusCode() >= 300) {
+        throw new HttpResponseException(statusLine.getStatusCode(), statusLine.getReasonPhrase());
+      }
+    }
   }
 }
